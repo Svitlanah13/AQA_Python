5e2818da03be15275f907840a5fcfe052b722b53
svitlanahnatiuk <svitlanag99@gmail.com>
2024-05-14 12:40:34 +0300

    Виправлення до Домашня робота №14.

--- Test/homework_14/tests/test_store_facade.py
+++ Test/homework_14/tests/test_store_facade.py
@@ -1,33 +1,24 @@
 import pytest
-from src.store.store_facade import StoreFacade, Phone
-
-@pytest.fixture
-def store():
-    return StoreFacade()
-
-@pytest.fixture
-def phone():
-    return Phone("TestModel", 1000)
 
 @pytest.mark.add
-def test_add_phone_to_catalog(store, phone):
-    store.add_phone_to_catalog(phone.model, phone.price)
+def test_add_phone_to_catalog(store_with_phone):
+    store, phone = store_with_phone
     catalog = store.show_catalog()
     assert len(catalog) == 1
     assert catalog[0].model == phone.model
     assert catalog[0].price == phone.price
 
 @pytest.mark.remove
-def test_remove_phone_from_catalog(store, phone):
-    store.add_phone_to_catalog(phone.model, phone.price)
+def test_remove_phone_from_catalog(store_with_phone):
+    store, phone = store_with_phone
     removed_phone = store.remove_phone_from_catalog(phone.model)
     assert removed_phone.model == phone.model
     assert removed_phone.price == phone.price
     assert len(store.show_catalog()) == 0
 
 @pytest.mark.show
-def test_show_catalog(store, phone):
-    store.add_phone_to_catalog(phone.model, phone.price)
+def test_show_catalog(store_with_phone):
+    store, phone = store_with_phone
     catalog = store.show_catalog()
     assert len(catalog) == 1
     assert catalog[0].model == phone.model
@@ -38,10 +29,11 @@ def test_show_catalog_empty(store):
     catalog = store.show_catalog()
     assert len(catalog) == 0
 
+@pytest.mark.parametrize("model", [
+    "NonExistentModel",
+    "TestModel1",
+])
 @pytest.mark.remove
-def test_remove_nonexistent_phone(store):
-    try:
-        removed_phone = store.remove_phone_from_catalog("NonExistentModel")
-        assert removed_phone is None
-    except IndexError:
-        assert True
+def test_remove_nonexistent_phone(store, model):
+    with pytest.raises(IndexError):
+        store.remove_phone_from_catalog(model)
